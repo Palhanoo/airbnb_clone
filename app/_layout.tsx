e9fde593b0e3c15134a55e005190bf989935db54
Palhanoo <bpalhano00@gmail.com>
2024-03-21 10:35:19 +1100

    booking page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,8 @@ import { TouchableOpacity } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
 import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import ModalHeaderText from '@/components/ModalHeaderText';
+import Colors from '@/constants/Colors';
 
 const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
@@ -110,8 +112,19 @@ function RootLayoutNav() {
           presentation: 'transparentModal',
           animation: 'fade',
           headerTransparent: true,
+          headerTitle: () => <ModalHeaderText />,
+          headerBackVisible: false,
           headerLeft: () => (
-            <TouchableOpacity onPress={() => router.back()}>
+            <TouchableOpacity 
+              onPress={() => router.back()}
+              style={{
+                backgroundColor: '#fff',
+                borderColor: Colors.grey,
+                borderRadius: 20,
+                borderWidth: 1,
+                padding: 4
+              }}
+            >
               <Ionicons name="close-outline" size={24} />
             </TouchableOpacity>
           ),
